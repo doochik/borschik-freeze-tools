a6e509b7c60edc17c64127a9ef97476e907800b0
Alexey Androsov <doochik@ya.ru>
2013-03-23 18:06:01 +0200

    freeze files examples

--- examples/freeze-files/1.js
+++ examples/freeze-files/1.js
@@ -1,12 +1,13 @@
 var freeze = /* borschik:include:freeze.files.json */;
 for (var i in freeze) {
-    var modUrl = '_freeze/' + freeze[i].replace(/\.js$/, '');
+    var modUrl = freeze[i].replace(/\.js$/, '');
     var modName = i.replace(/.*\//, '').replace(/\.js$/, '');
     freeze[modName] = modUrl;
     delete freeze[i];
 }
 
 require.config({
+    baseUrl: '_freeze',
     paths: freeze
 });
 
--- examples/freeze-files2/1.js
+++ examples/freeze-files2/1.js
@@ -1,5 +1,10 @@
+var freeze = /* borschik:include:ent.freeze.json */;
+for (var i in freeze) {
+    freeze[i] = freeze[i].replace(/\.js$/, '')
+}
+
 require.config({
-    paths: /* borschik:include:ent.freeze.json */
+    paths: freeze
 });
 
 require(['module1', 'module2'], function() {
